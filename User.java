c92d878b66b3584a6adca7a522a0b1e83bedd1be
Lealtad-Was-Here <discordnomebanees932@gmail.com>
2025-12-11 20:06:36 -0300

    Create User.java

---
+++ User.java
@@ -0,0 +1,29 @@
+package org601.entity;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+
+    public User() {}
+    public User(String username, String password, Set<String> roles) {
+        this.username = username; this.password = password; this.roles = roles;
+    }
+    public Long getId() { return id; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public Set<String> getRoles() { return roles; }
+    public void setPassword(String password) { this.password = password; }
+}
